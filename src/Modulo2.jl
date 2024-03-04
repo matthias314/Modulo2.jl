8beb8e95cfef4805aa86b69f672dad6b58587741
matthias314 <matthias314@posteo.net>
2024-03-04 18:58:09 -0500

    added docstring for the module

--- src/Modulo2.jl
+++ src/Modulo2.jl
@@ -1,3 +1,14 @@
+"""
+    Modulo2
+
+This package provides types and functions for linear algebra modulo 2.
+It defines a type `ZZ2` for integers mod 2 and
+`ZZ2Array` (`ZZ2Vector`, `ZZ2Matrix`) for arrays (vectors, matrices)
+with elements in `ZZ2`.
+
+See also [`ZZ2`](@ref), [`ZZ2Array`](@ref), [`det`](@ref), [`inv`](@ref),
+[`rank`](@ref), [`rcef`](@ref), [`rref`](@ref).
+"""
 module Modulo2
 
 import Base: show, ==, +, -, *, /, ^, inv, literal_pow,
