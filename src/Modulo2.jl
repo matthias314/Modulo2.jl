afc25fa1daedf074331aaf60f3fd5430ce5e7dc9
matthias314 <matthias314@posteo.net>
2024-03-27 16:30:47 -0400

    added conversion to `Bool` to doctring for `ZZ2`

--- src/Modulo2.jl
+++ src/Modulo2.jl
@@ -40,6 +40,7 @@ Elements can be created via the functions `zero` and `one` or
 from an argument of type `Bool` or any other `Integer` type.
 More generally, any argument accepted by `isodd` can be converted to `ZZ2`.
 In algebraic computations with `ZZ2`, `Integer` types are promoted to `ZZ2`.
+Conversely, elements of type `ZZ2` can be converted to `Bool`.
 
 See also `Base.zero`, `Base.one`, `Base.iszero`, `Base.isone`, `Base.isodd`.
 
@@ -51,11 +52,14 @@ true
 julia> iszero(ZZ2(4.0))
 true
 
-julia> ZZ2(1) + 3
+julia> x = ZZ2(1) + 3
 0
 
-julia> typeof(ans)
+julia> typeof(x)
 ZZ2
+
+julia> Bool(x), convert(Bool, x)
+(false, false)
 ```
 """
 struct ZZ2 <: Number
